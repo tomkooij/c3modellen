7390c7bf760ebccbf40bf455bffd3b1de7202513
Tom Kooij <tomkooij@tomkooij.nl>
2019-09-18 15:15:54 +0200

    Kogelbaan met luchtweerstand

--- modellen/models.js
+++ modellen/models.js
@@ -3,5 +3,6 @@
 var model_index = [
     {url:'modellen/horizontale_worp.xml', title: 'Horizontale worp'},
     {url:'modellen/kogelbaan.xml', title: 'Kogelbaanmodel'},
+    {url:'modellen/kogelbaan_luchtweerstand.xml', title: 'Kogelbaanmodel met luchtweerstand'},
     {url:'modellen/satelliet.xml', title: 'Satiellietmodel'},
 ];
