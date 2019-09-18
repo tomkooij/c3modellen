2a33c81cac5206d77f5b621bd01ca2ce7509fa59
Tom Kooij <tomkooij@tomkooij.nl>
2019-09-18 11:58:12 +0200

    Add horizontale worp

--- modellen/models.js
+++ modellen/models.js
@@ -1,6 +1,7 @@
 // definieer hier de modellen
 
 var model_index = [
+    {url:'modellen/horizontale_worp.xml', title: 'Horizontale worp'},
     {url:'modellen/kogelbaan.xml', title: 'Kogelbaanmodel'},
     {url:'modellen/satelliet.xml', title: 'Satiellietmodel'},
 ];
