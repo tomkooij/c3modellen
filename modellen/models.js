11f710a2f27ec3164394b704c97efc17462e4515
Tom Kooij <tomkooij@tomkooij.nl>
2019-09-18 11:22:55 +0200

    Kogelbaan model

--- modellen/models.js
+++ modellen/models.js
@@ -1,22 +1,6 @@
 // definieer hier de modellen
 
 var model_index = [
-    {url:'modellen/leeg.xml', title: 'Lege modelomgeving'},
-    {url:'modellen/constantesnelheid.xml', title: 'Constante snelheidsmodel'},
-    {url:'modellen/constanteversnelling.xml', title: 'Constante versnellingsmodel'},
-    {url:'modellen/krachtversnelling.xml', title: 'Kracht-versnellingsmodel'},
-    {url:'modellen/voorbeeld.xml', title: 'Simpel voorbeeldmodel'},
-    {url:'modellen/opgave_auto.xml', title: 'Opgave auto (antwoord)'},
-    {url:'modellen/opgave_schaatser.xml', title: 'Opgave schaatser (antwoord)'},
-    {url:'modellen/opgave_skydiver.xml', title: 'Opgave skydiver (antwoord)'},
-    {url:'modellen/opgave_fietser.xml', title: 'Opgave fietser (antwoord)'},
-    {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave voetbal verticaal'},
-    {url:'modellen/opgave_x-stream.xml', title: 'Opgave X-Stream'},
-    {url:'modellen/opgave_optrekkenmetconstantekracht.xml', title: 'Opgave optrekken met constante kracht'},
-    {url:'modellen/opgave_optrekkenmetconstantekracht_antwoord.xml', title: 'Opgave optrekken met constante kracht (antwoord)'},
-    {url:'modellen/opgave_optrekkenmetconstantvermogen.xml', title: 'Opgave optrekken met constant vermogen'},
-    {url:'modellen/opgave_optrekkenmetconstantvermogen_antwoord.xml', title: 'Opgave optrekken met constant vermogen (antwoord)'},
-    {url:'modellen/opgave_uitrijdenvaneenauto.xml', title: 'Opgave uitrijden van een auto'},
-    {url:'modellen/opgave_buckeyebullet.xml', title: 'Opgave Buckeye bullet'},
-
+    {url:'modellen/kogelbaan.xml', title: 'Kogelbaanmodel'},
+    {url:'modellen/satelliet.xml', title: 'Satiellietmodel'},
 ];
